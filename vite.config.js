edc573cc87cc429a4e989bda4d92f28a1fd10e61
Thomas Mach <m4chtomasz@gmail.com>
2025-03-16 17:36:44 +0100

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import removeConsole from "vite-plugin-remove-console";
-
-export default defineConfig({
-  plugins: [vue(), removeConsole()],
-  base: "/auth-app-frontend/",
-  build: {},
-});
